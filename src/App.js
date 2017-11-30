53bab09940f2dcb1f4028512ac73129ae700ab7d
Nick <soda@dhcp-v082-203.mobile.uci.edu>
2017-11-29 21:27:03 -0800

    pushing test code

--- src/App.js
+++ src/App.js
@@ -1,21 +1,50 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import axios from 'axios';
 
-class App extends Component {
+var config = {
+  headers: {
+    // 'crossDomain': true, 
+    'Content-Type': 'application/x-www-form-urlencoded',
+    'Access-Control-Allow-Origin': '*'
+    }
+};
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      posts: []
+    };
+  }
+
+  componentDidMount() {
+    axios.get(`https://quandl.com/api/v3/datatables/WIKI/PRICES?ticker=${this.props.ticker}&date=2017-11-16&api_key=zyrYsZstgPbhDmNUupNy`, config)
+    // axios.get(`http://www.reddit.com/r/${this.props.subreddit}.json`)
+      .then(res => {
+        const posts = res.data.data.children.map(obj => obj.data);
+        this.setState({ posts });
+      });
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <h1>{`/r/${this.props.ticker}`}</h1>
+        <ul>
+          {this.state.posts.map(post =>
+            <li key={post.id}>{post.title}</li>
+          )}
+        </ul>
       </div>
     );
   }
 }
+
+ReactDOM.render(
+  <App ticker="AAPL"/>,
+  document.getElementById('root')
+);
 
 export default App;
