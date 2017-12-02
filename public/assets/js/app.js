0da76e526a55d140df2cb1167c46fc4fa50c46a5
Nick <soda@dhcp-v101-192.mobile.uci.edu>
2017-12-02 13:58:28 -0800

    added minor commments

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -13,12 +13,13 @@ function getStockData() {
     ticker = document.getElementById('ticker').value;
     axios.get(`https://api.iextrading.com/1.0/stock/` + ticker + `/quote`, config)
     .then(res => {
-        console.log("Company Name: " + res.data.companyName);
-        console.log("Stock Ticker: " + res.data.symbol);
-        console.log("Sector: " + res.data.sector);
-        console.log("Open Price: " + res.data.open);
-        console.log("Close Price: " + res.data.close);
+        console.log("Company Name: " + res.data.companyName); // store this
+        console.log("Stock Ticker: " + res.data.symbol); // store this
+        console.log("Sector: " + res.data.sector); // store this
+        console.log("Open Price: " + res.data.latestPrice); // store this
+        console.log("Open Price: " + res.data.open); 
+        console.log("Close Price: " + res.data.close); 
         console.log("Date: " + res.data.latestTime);
-        console.log("Epoc Date: " + res.data.latestUpdate);
+        console.log("Epoc Date: " + res.data.latestUpdate); // store this
     });
 }
