dab1b25e9a92ab93f4a007a1da73febcc9c00728
Nick <nickui@users.noreply.github.com>
2017-12-02 10:44:31 -0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -21,49 +21,7 @@ class App extends React.Component {
   }
 
   componentDidMount() {
-  //   axios.get(`https://cors-anywhere.herokuapp.com/https://www.quandl.com/api/v3/datatables/WIKI/PRICES?ticker=${this.props.ticker}&date=2017-11-16&api_key=zyrYsZstgPbhDmNUupNy`)
-  //   .then(res => {
-  //     console.log(res.data)
-  // })
-  //   .catch(function (error) {
-  //     console.log(error);
-  // });
-    
-    // const axios = require("axios");
-    // const url =
-    //   `https://quandl.com/api/v3/datatables/WIKI/PRICES?ticker=AAPL&date=2017-11-16&api_key=zyrYsZstgPbhDmNUupNy`;
-    // axios
-    //   .get(url)
-    //   .then(response => {
-    //     console.log(
-    //       `City: ${response.data.results[0]} -`,
-    //       `Latitude: ${response.data.results[0].geometry.location.lat} -`,
-    //       `Longitude: ${response.data.results[0].geometry.location.lng}`
-    //     );
-    //   })
-    //   .catch(error => {
-    //     console.log(error);
-    //   });
-    
-    
-    // No third party module required: https is part of the Node.js API
-    // const https = require("https");
-    // const url =
-    //   `https://quandl.com/api/v3/datatables/WIKI/PRICES?ticker=${this.props.ticker}&date=2017-11-16&api_key=zyrYsZstgPbhDmNUupNy`;
-    // https.get(url, res => {
-    //   res.setEncoding("utf8");
-    //   let body = "";
-    //   res.on("data", data => {
-    //     body += data;
-    //   });
-    //   res.on("end", () => {
-    //     body = JSON.parse(body);
-    //     console.log(
-    //       `Data: ${body.results[0].datatable} -`
-    //     );
-    //   });
-    // });
-    // GET request for remote image
+    // GET request for stock data
 
     axios.get(`https://api.iextrading.com/1.0/stock/aapl/quote`, config)
     // axios.get(`https://cors-anywhere.herokuapp.com/https://quandl.com/api/v3/datatables/WIKI/PRICES/?ticker=${this.props.ticker}&date=2017-11-16&api_key=zyrYsZstgPbhDmNUupNy`, config)
