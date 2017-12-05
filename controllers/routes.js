aaee114fa9abee6e726c374c9dc730df17e21716
maike-hilda <maikescherer@gmail.com>
2017-12-04 20:00:30 -0800

    added routes

--- controllers/routes.js
+++ controllers/routes.js
@@ -1,6 +1,7 @@
+// routes that do not require authorization
 // set up express 
 var express = require("express");
-var burger = require("../models/burger.js");
+var path = require("path");
 
 var router = express.Router();
 
@@ -8,25 +9,27 @@ var router = express.Router();
 
 // base route
 router.get("/", function(req, res) {
-    burger.all(function(burgerData) {
-        console.log(burgerData);
-        res.render("index", {burgerData});
-    });
+    res.sendFile(path.join(__dirname, "/../public/home.html"));
 });
 
-// to update burgers after devouring
-router.put("/burgers/update", function(req, res) {
-    burger.update(req.body.burger_id, function(result) {
-        console.log(result);
-        res.redirect("/");
-    });
+router.get("/about", function(req, res) {
+    res.sendFile(path.join(__dirname, "/../public/about.html"));
 });
 
-// add using submit button
-router.post("/burgers/create", function(req, res) {
-    burger.create(req.body.burger_name, function(result) {
-        res.redirect("/");
-    });
+router.get("*", function(req, res) {
+    res.redirect("/");
 });
+
+// // non-auth related routes
+// app.get("/about", authController.about);
+// app.get("*", authController.default);
+
+// // not auth routes
+// exports.about = function(req, res) {
+// res.sendFile(path.join(__dirname, "/../public/about.html"));
+// };
+// exports.default = function(req, res) {
+// res.redirect("/");
+// };
 
 module.exports = router;
--- server.js
+++ server.js
@@ -3,22 +3,63 @@ var express = require("express");
 var bodyParser = require("body-parser");
 var methodOverride = require("method-override");
 var handlebars = require("express-handlebars");
-var routes = require("./controllers/routes.js");
-
+// https://code.tutsplus.com/tutorials/using-passport-with-sequelize-and-mysql--cms-27537
+// assigns express module to express
+var express = require('express');
+// initialize express
 var app = express();
-app.use(express.static(__dirname + "/public"));
+// import needed modules for authentication
+var passport   = require('passport');
+var session    = require('express-session');
+// to extract body part of incoming request to JSON
+var bodyParser = require('body-parser');
+// environment variables
+var env = require("dotenv").load();
 
-app.use(bodyParser.urlencoded({
-    extended: false
-}));
+// Middleware
+// to allow app to use bodyParser
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
-app.use(methodOverride("_method"));
-app.engine("handlebars", handlebars({
-    defaultLayout: "main"
-}));
-app.set("view engine", "handlebars");
+// initialize passport and express session 
+app.use(session( { 
+    secret: 'keyboard cat',
+    resave: true, 
+    saveUninitialized:true
+})); // session secret
+app.use(passport.initialize());
+app.use(passport.session()); // persistent login sessions
 
+// app.get('/', function(req, res) {
+//     res.send('Welcome to Passport with Sequelize');
+// });
+
+//Models
+var models = require("./app/models");
+
+//Routes
+var routes = require("./app/routes/routes.js");
+var authRoute = require('./app/routes/auth.js')(app, passport);
+
+// initialize routes
 app.use("/", routes);
 
+//load passport strategies
+require('./app/config/passport/passport.js')(passport, models.user);
+
+//Sync Database
+models.sequelize.sync().then(function() {
+    console.log('Nice! Database looks fine')
+}).catch(function(err) {
+    console.log(err, "Something went wrong with the Database Update!")
+});
+
+// start server
 var port = 3000;
-app.listen(port);
+app.listen(port, function(err) {
+    if (!err) {
+        console.log("Listening on port", port);
+    } else {
+        console.log(err);
+    }
+});
