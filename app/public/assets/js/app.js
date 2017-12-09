f55e8812b808cc9e16da7c955b4c3b97e1b71efa
Nick <soda@wsip-174-66-87-21.oc.oc.cox.net>
2017-12-08 18:04:43 -0800

    updated jquery to min from slim

--- app/public/assets/js/app.js
+++ app/public/assets/js/app.js
@@ -33,7 +33,7 @@ var config = {
           stockPriceCell.innerHTML = res.data.latestPrice;
           var buyButtonCell = document.createElement("td");
           // buyStock('APPL', 1.23, 1234553);
-          buyButtonCell.innerHTML = '<button type="button" class="btn btn-default" onclick="buyStock(\'' + res.data.symbol + '\', ' + res.data.latestPrice + ', ' + res.data.latestUpdate + ');">Buy</button>';
+          buyButtonCell.innerHTML = '<button type="button" class="btn btn-primary" onclick="buyStock(\'' + res.data.symbol + '\', ' + res.data.latestPrice + ', ' + res.data.latestUpdate + ');">Buy</button>';
           var row = document.createElement("tr");
           row.appendChild(companyNameCell);
           row.appendChild(stockPriceCell);
@@ -62,7 +62,7 @@ var config = {
           // user.stocks (each stock has a ticker, price, currentStockBuyDate, quantity)
           var stock = {
               ticker: currentStockTicker,
-              companyName: companyName,
+              // companyName: companyName,
               // sector: currentSector,
               price: currentStockPrice,
               epocDate: currentStockBuyDate,
